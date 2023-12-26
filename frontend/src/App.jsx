b5c0754db1f25ea286af9cbd98e643427183cd55
Jovan Brzakovic <jova3105@gmail.com>
2023-12-26 16:51:42 +0100

    Setting up routes Login/Register and Main page using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Main from "./Pages/Main"
 
 function App() {
 
   return (
     <>
-      Activity tracker app
+      <Routes>
+        <Route path="/" element={<Main></Main>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+      </Routes>
     </>
   )
 }
