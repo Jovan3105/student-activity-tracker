38be6c03ba1b60fe04f4b7a9f207bf0b44a5eb16
Jovan Brzakovic <jova3105@gmail.com>
2024-02-23 15:57:57 +0100

    Game filtering through the calendar with react-datepicker.

--- frontend/src/Pages/AllGames/AllGames.jsx
+++ frontend/src/Pages/AllGames/AllGames.jsx
@@ -4,17 +4,24 @@ import { useEffect, useState } from "react";
 import { baseUrl, getRequest } from "../../utils/services";
 import { useParams } from "react-router-dom";
 import moment from "moment";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 const AllGames = () => {
     const { quizId } = useParams();
     const [showModal, setShowModal] = useState(false);
     const [games, setGames] = useState([]);
     const quiz = JSON.parse(sessionStorage.getItem("Quiz"));
     const [modalGameResult, setModalGameResult] = useState(null);
+    const [dateRange, setDateRange] = useState([null, null]);
+    const [startDate, endDate] = dateRange;
+    const [filteredData, setFilteredData] = useState([]);
+
 
     useEffect(() => {
         const response = getRequest(`${baseUrl}/games/${quizId}/games`).then((value) => {
             //console.log(value);
             setGames(value);
+            setFilteredData(value);
         })
     }, []);
 
@@ -25,14 +32,56 @@ const AllGames = () => {
             setModalGameResult(value.results);
         })
     }
+    const handleSearch = (dataRange) => {
+        if (!dataRange[0] && !dataRange[1]) {
+            setFilteredData(games);
+        }
+        
+        if (dataRange[0] && dataRange[1]) {
+            const filtered = games.filter(item => {
+                const itemDate = new Date(item.createdAt).getTime();
+                return itemDate >= dataRange[0].getTime() && itemDate <= dataRange[1].getTime();
+            });
+            setFilteredData(filtered);
+        }
+
+        setDateRange(dataRange);
+    };
 
     return (
         <>
             <div className="row justify-content-center">
-                <div className="col-md-10 mb-2">
+                <div className="col-md-10 col-lg-5 col-xxl-5 mb-3">
                     <h3 className="text-center">{quiz.name}</h3>
                 </div>
             </div>
+            <div className="row justify-content-center">
+                <div className="col-md-10 col-lg-5 col-xxl-5 text-center" >
+                    <DatePicker
+                        showIcon
+                        placeholderText="Click to select"
+                        selectsRange={true}
+                        startDate={startDate}
+                        endDate={endDate}
+                        onChange={(update) => {
+                            handleSearch(update);
+                        }}
+                        isClearable={true}
+                        icon={
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" style={{ top: "3px" }}>
+                                <mask id="ipSApplication0">
+                                    <g fill="none" stroke="#fff" strokeLinejoin="round" strokeWidth="4">
+                                        <path strokeLinecap="round" d="M40.04 22v20h-32V22"></path>
+                                        <path fill="#fff" d="M5.842 13.777C4.312 17.737 7.263 22 11.51 22c3.314 0 6.019-2.686 6.019-6a6 6 0 0 0 6 6h1.018a6 6 0 0 0 6-6c0 3.314 2.706 6 6.02 6c4.248 0 7.201-4.265 5.67-8.228L39.234 6H8.845l-3.003 7.777Z"></path>
+                                    </g>
+                                </mask>
+                                <path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSApplication0)"></path>
+                            </svg>
+                        }
+                    />
+                </div>
+            </div>
+            <br />
             <div className="row justify-content-center">
                 <div className="col-md-10 mb-3">
                     <div className="card">
@@ -40,7 +89,7 @@ const AllGames = () => {
                             <div className="row g-2">
                                 {
                                     games.length != 0 ?
-                                        games.sort(function (a, b) { return new Date(b.createdAt) - new Date(a.createdAt) }).map((game, index) => (
+                                        filteredData.sort(function (a, b) { return new Date(b.createdAt) - new Date(a.createdAt) }).map((game, index) => (
                                             <div className="col-12 col-lg-3 col-xxl-2 px-3 py-3" key={game._id} onClick={() => showGame(game._id)}>
                                                 <div className="card mx-auto element-game">
                                                     <div className="card-body">
